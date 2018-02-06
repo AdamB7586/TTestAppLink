e58c044c972394b8bd34cad8d13ee7aee08332a2
Adam Binnersley <abinnersley@gmail.com>
2018-02-06 10:59:37 +0000

    Add unit testing

---
+++ tests/AppLinkTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace AppLink\Tests;
+
+use PHPUnit\Framework\TestCase;
+use DBAL\Database;
+use UserAuth\User;
+use Smarty;
+use AppLink\AppLink;
+
+class AppLinkTest extends TestCase{
+    
+    protected $db;
+    protected $template;
+    protected $user;
+    protected $appLink;
+
+    protected function setUp() {
+        $this->db = new Database($hostname, $username, $password, $database);
+        $this->template = New Smarty();
+        $this->user = new User($this->db);
+        $this->appLink = new AppLink($this->db, $this->template, $this->user);
+    }
+    
+    protected function tearDown() {
+        $this->appLink = null;
+    }
+    
+    /**
+     * @covers AppLink\AppLink::setDataUrl
+     */
+    public function testSetDataUrl(){
+        $this->markTestIncomplete();
+    }
+}
